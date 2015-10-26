c3ffb9423e7937b56bc16f8ce07ebf1dba05c7ac
Phan Nguyen <phannguyen2020@gmail.com>
2015-10-26 10:10:24 +0700

    add acl file

---
+++ apps/config/acl.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * acl.php 26/10/2015
+ * ----------------------------------------------
+ *
+ * @author      Phan Nguyen <phannguyen2020@gmail.com>
+ * @copyright   Copyright (c) 2015, framework
+ *
+ * ----------------------------------------------
+ * All Rights Reserved.
+ * ----------------------------------------------
+ */
+class acl
+{
+
+}
